143535943e10c819517fc1e2fa3b8a41ac55490a
HangDh <hangiedh@gmail.com>
2017-11-14 18:12:02 +0100

    Started FB Authen -> but need to update laptop, to proceed later on

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,12 @@ namespace AspNetCoreTodo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication().AddFacebook(options =>
+                {
+                options.AppId = Configuration["Facebook:AppId"];
+                options.AppSecret = Configuration["Facebook:AppSecret"]
+                });
+                
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
